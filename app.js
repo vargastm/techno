4fbb89600aa657bce6ba51a976d8564f4e1284a0
Tiago Martins <vargas.tiago@hotmail.com>
2023-07-04 17:44:05 -0300

    Added styles to modal and created cart

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const vm = new Vue({
   el:"#app",
   data: {
     products: [],
-    product: false
+    product: false,
+    cart:[]
    },
    filters: {
     numberPrice(value) {
       return parseFloat(value).toLocaleString("pt-BR", { style: "currency", currency: "BRL"});
+    }
+   },
+   computed: {
+    cartTotal() {
+      let total = 0;
+      if(this.cart.length) {
+        this.cart.forEach(item => {
+          total += item.preco;
+        })
+      }
+      return total;
     }
    },
   methods: {
@@ -23,9 +35,38 @@ const vm = new Vue({
       .then(r => {
         this.product = r;
       })
+    },
+    openModal(id) {
+      this.fetchProduct(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    closeModal({target, currentTarget}) {
+      if (target === currentTarget) this.product = false;
+    },
+    addItem() {
+      this.product.estoque--
+      const{ id, nome, preco } = this.product;
+      this.cart.push({id, nome, preco})
+    },
+    removeItem(index) {
+      this.cart.splice(index, 1)
+    },
+    checkLocalStorage() {
+      if(window.localStorage.cart) {
+        this.cart = JSON.parse(window.localStorage.cart);
+      }
+    }
+  },
+  watch: {
+    cart() {
+      window.localStorage.cart = JSON.stringify(this.cart);
     }
   },
   created() {
     this.fetchProducts();
+    this.checkLocalStorage();
   }
 })
